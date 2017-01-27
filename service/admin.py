ef4017d813fb08db554cc7b9fe886e10505bf0e4
cholomia <pocholomia@gmail.com>
2017-01-27 16:51:29 +0800

    friday

--- service/admin.py
+++ service/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Classification, Job
 
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Classification)
+admin.site.register(Job)
--- service/models.py
+++ service/models.py
@@ -18,3 +18,46 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class UserRatingAvail(models.Model):
+    user = models.ForeignKey('auth.User', related_name='user_rating_avail', on_delete=models.CASCADE)
+    rate = models.IntegerField()
+    comment = models.CharField(max_length=100)
+
+
+class UserRatingRender(models.Model):
+    user = models.ForeignKey('auth.User', related_name='user_rating_render', on_delete=models.CASCADE)
+    rate = models.IntegerField()
+    comment = models.CharField(max_length=100)
+
+
+class Classification(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Job(models.Model):
+    user = models.ForeignKey('auth.User', related_name='user_jobs', on_delete=models.CASCADE)
+    classification = models.ForeignKey(Classification, related_name='job_classification', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    date_start = models.CharField(max_length=8)
+    date_end = models.CharField(max_length=8)
+    fee = models.FloatField()
+    location = models.CharField(max_length=100)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class JobApplication(models.Model):
+    user = models.ForeignKey('auth.User', related_name='forums', on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, related_name='job_applications', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    accept = models.BooleanField(default=False)
